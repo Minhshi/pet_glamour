c465b330c66dac04c9058d6a77a098aa3bb0aaf7
Minh Pham <mpham@naver.com>
2018-12-28 18:06:27 +0100

    Update migration file for booking model

--- db/migrate/20181228154347_create_bookings.rb
+++ db/migrate/20181228154347_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
       t.string :state
-      t.monetize :amount
+      t.monetize :amount, currency: { present: false }
       t.jsonb :payment
       t.references :user, foreign_key: true
 
