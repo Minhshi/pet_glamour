b7d7cd45fe7b55544eb51b34532e423071f7769f
Minh Pham <mpham@naver.com>
2018-12-28 18:08:23 +0100

    Add new and create routes for payments

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # root to: 'pages#home'
   root 'services#index'
   resources :services, only: [:index, :show]
+  resources :bookings, only: [:show, :create] do
+    resources :payments, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
