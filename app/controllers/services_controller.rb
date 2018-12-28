cc12aa401f5d3a85a56530022eab3b69502839af
Minh Pham <mpham@naver.com>
2018-12-28 15:23:20 +0100

    Add controller for service model

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,11 @@
+class ServicesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @services = Service.all
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+end
