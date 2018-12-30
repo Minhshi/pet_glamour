bf111820c1f121224426e1bfc10764b3fe237e24
Minh Pham <mpham@naver.com>
2018-12-30 20:55:08 +0100

    Show booked service with image and custom stripe button

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,7 +5,7 @@ class BookingsController < ApplicationController
 
   def create
     service = Service.find(params[:service_id])
-    booking  = Booking.create!(service: service.name, amount: service.price, state: 'pending', user: current_user)
+    booking  = Booking.create!(service_name: service.name, service_photo: service.photo_url, amount: service.price, state: 'pending', user: current_user)
 
     redirect_to new_booking_payment_path(booking)
   end
