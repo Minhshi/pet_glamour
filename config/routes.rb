50f92e822dd6e438e4f61cd4a4911a6b860f8212
Minh Pham <mpham@naver.com>
2018-12-31 17:26:52 +0100

    Add route to user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :bookings, only: [:show, :create, :index] do
     resources :payments, only: [:new, :create]
   end
+  get 'profile', to: 'users#show', as: :profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
