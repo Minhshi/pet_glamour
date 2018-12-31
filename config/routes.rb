b12b07f8c763e6aec6fc595ba9d8920828c5997b
Minh Pham <mpham@naver.com>
2018-12-31 10:55:35 +0100

    Add bookings index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :services, only: [:index, :show]
-  resources :bookings, only: [:show, :create] do
+  resources :bookings, only: [:show, :create, :index] do
     resources :payments, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
