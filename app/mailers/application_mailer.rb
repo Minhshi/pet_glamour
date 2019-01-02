c94ea6e26e98e5cf1621d3eb68eccad72ba53153
Minh Pham <mpham@naver.com>
2019-01-02 16:58:16 +0100

    Add missing postmark configuration

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'hello@minhpham.me'
-  layout 'mailer'
+  # default from: 'hello@minhpham.me'
+  # layout 'mailer'
+  def message
+    mail(
+      :subject => 'Hello from Postmark',
+      :to  => 'hello@minhpham.me',
+      :from => 'hello@minhpham.me',
+      :html_body => '<strong>Hello</strong> dear Postmark user.',
+      :track_opens => 'true')
+  end
 end
