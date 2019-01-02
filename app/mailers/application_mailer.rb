5a84dd139f888fe3c968f4b7182fbb5c1bdfaa50
Minh Pham <mpham@naver.com>
2019-01-02 17:03:56 +0100

    Change back postmark configurations

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  # default from: 'hello@minhpham.me'
-  # layout 'mailer'
-  def message
-    mail(
-      :subject => 'Hello from Postmark',
-      :to  => 'hello@minhpham.me',
-      :from => 'hello@minhpham.me',
-      :html_body => '<strong>Hello</strong> dear Postmark user.',
-      :track_opens => 'true')
-  end
+  default from: 'hello@minhpham.me'
+  layout 'mailer'
 end
