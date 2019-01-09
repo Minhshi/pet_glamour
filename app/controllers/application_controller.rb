e48f11db125a138bfbbd8f876887ac8286af5718
Minh Pham <mpham@naver.com>
2019-01-09 17:12:17 +0100

    Add name to user model and ask for it upon registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :store_current_location, :unless => :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def store_current_location
     store_location_for(:user, request.url)
@@ -9,6 +10,12 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     request.referrer || root_path
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
