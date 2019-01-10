cca34d4648b8e3667e205b7da920cba154cd7539
Minh Pham <mpham@naver.com>
2019-01-10 16:37:59 +0100

    Disable weekends plus show external elements for flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,17 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
 
-flatpickr(".datepickr", {
+flatpickr(
+  ".datepicker", {
+   altInput: true,
+   altFormat: "F j, Y",
+   dateFormat: "Y-m-d",
+   wrap: true,
+   "disable": [
+        function(date) {
+            // return true to disable
+            return (date.getDay() === 5 || date.getDay() === 6);
 
-})
+        }
+    ],
+ })
